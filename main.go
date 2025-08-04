6b07a6b4698e15bbc04db1f0064f90169f40ff2a
Blas Yaselli <byaselli21@gmail.com>
2025-08-04 03:56:34 -0600

    execute command

--- main.go
+++ main.go
@@ -1,41 +1,45 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
 	"github.com/charmbracelet/wish/logging"
 )
 
 const (
-	host = "localhost"
+	host = "0.0.0.0"
 	port = "22"
 )
 
 func main() {
-	srv, err := wish.NewServer(
-		// The address the server will listen to.
+	s, err := wish.NewServer(
 		wish.WithAddress(net.JoinHostPort(host, port)),
-
-		// The SSH server need its own keys, this will create a keypair in the
-		// given path if it doesn't exist yet.
-		// By default, it will create an ED25519 key.
 		wish.WithHostKeyPath(".ssh/id_ed25519"),
-
-		// Middlewares do something on a ssh.Session, and then call the next
-		// middleware in the stack.
 		wish.WithMiddleware(
 			func(next ssh.Handler) ssh.Handler {
-				return func(sess ssh.Session) {
-					wish.Println(sess, "Hello, world!")
-					next(sess)
+				return func(s ssh.Session) {
+					cmd := wish.Command(s, "bash", "ai chat")
+					if err := cmd.Run(); err != nil {
+						wish.Fatalln(s, err)
+					}
+					next(s)
 				}
 			},
-
-			// The last item in the chain is the first to be called.
+			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
 			logging.Middleware(),
 		),
 	)
@@ -43,9 +47,21 @@ func main() {
 		log.Error("Could not start server", "error", err)
 	}
 
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	log.Info("Starting SSH server", "host", host, "port", port)
-	if err = srv.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
-		// We ignore ErrServerClosed because it is expected.
-		log.Error("Could not start server", "error", err)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
 	}
 }
